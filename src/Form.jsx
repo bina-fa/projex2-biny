b65537f4a710eef27d5a21f5a580631261d4624f
Biny <rachelfa33@gmail.com>
2024-12-24 21:24:41 +0200

    Datalist seems OK

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,45 +2,47 @@ import { useForm } from "react-hook-form";
 import "./css/form.css"
 import { useState } from "react";
 
-
   
 function Form() {
 
 
-
-   let possibleAdresses = []
    let { register, reset, handleSubmit, formState: { errors, isValid }} = useForm();
-   let [ query, setQuery ] = useState([]);
-
-
-
-
-  
-
-   
+   let [ query, setQuery ] = useState();
+   let [ data, setData] = useState([{}]);
 
  function save(){
     reset()
  }
+  // function updateMap(){
+  //   <Map/>
+  // }
  
- function ChangeAdress(e){
-   console.log("go in on change")
+    const ChangeAdress = async(e) => {
+   console.log("go in on change");
+   console.log( e.target.value );
    setQuery(e.target.value);
-   fetch( `https://nominatim.openstreetmap.org/search?format=json&q=${query}&limit=5` )
-   .then( 
-       (res) =>{res = res.json()
-         console.log(res)
-         console.log(res)
 
-         // for (let i = 0; i < res.length; i++) {
-         //    possibleAdresses + res[i] ;
-            
-         // }
-         // console.log( "possibleAdresses" + possibleAdresses[0])
-       })
-        
+  //  fetch( `https://nominatim.openstreetmap.org/search?format=json&q=${query}&limit=5` )//איך ניגשים לתוך המערך שנמצא באובייקט promise
+  //  .then( 
+  //      (res) => {res = res.json();
+  //        console.log(res);
+  //        setdata(res);
+  //        console.log("addressArray", res.arrayBuffer)
+  //        console.log("data: ", data);
+  //        const addressArray = res.data; 
+  //        console.log("addressArray", addressArray);
+
+  //      })
+     
+       const response = await fetch( ` https://nominatim.openstreetmap.org/search?format=json&q=${query}&limit=5`)
+       const res = await response.json()
+       console.log("res: " , res);
+       setData(res);
+       console.log("data : " , data);
+       
  }
-    
+  
+
     return ( 
          <>
     <form noValidate onSubmit={handleSubmit(save)}>
@@ -49,8 +51,26 @@ function Form() {
           {errors.name && <div className="error">{errors.name.message} </div>}
 
 
-       <input type="text" {...register ( "adress", { required : 'שדה כתובת הוא שדה חובה'  })}onChange={ChangeAdress}  id ="adressInput" placeholder="adress"></input> 
+       <input
+        type="text"
+         {...register ( "adress", { required : 'שדה כתובת הוא שדה חובה'  })}
+         onChange={ChangeAdress}  
+         id ="adressInput"
+         list="options"
+          placeholder="adress">
+            </input> 
        {errors.adress && <div className="error">{errors.adress.message} </div>}
+
+
+       <datalist id = "options" >
+        {data.map(
+          (item) => ( 
+            <option key={item.osm_id} >
+              {item.display_name}
+              </option>
+        ) )}
+       </datalist>
+       {/* onClick={ <Map lat ={item.lat} lon = {item.lon}/> } */}
 
        <input type="phone" {...register ( "phone", { required :'שדה טלפון הוא שדה חובה'  })} id ="phoneInput" placeholder="phone"></input> 
        {errors.phone && <div className="error">{errors.phone.message} </div>}
@@ -66,13 +86,13 @@ function Form() {
              id="emailInput" placeholder="Email"></input> 
         {errors.email && <div className="error">{errors.email.message} </div>}
 
-       <label htmlFor="internet" value = "need connecting to internet?"/>
+       <label htmlFor="internet" >{"Need an internet connection?"}</label>
        <input type="Checkbox" name="internet" id="internet"/>
 
-       <label htmlFor="kitchen" value = "need a kitchen?"/> 
+       <label htmlFor="kitchen">{"Need a kitchen?"}</label> 
            <input type="Checkbox" name="kitchen" id="kitchen"/>
 
-       <label htmlFor="coffeeMachine" value = "need a coffe machine?"/>
+       <label htmlFor="coffeeMachine"> {"Need a coffe machine?"}</label>
               <input type="Checkbox"name="coffeeMachine" id="coffeeMachine"/>
 
        <input type="number" id="numRooms" defaultValue = {2} placeholder="num rooms"></input> 
---
+++ src/Map.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
+// function Map(props) {
+
+//     const lat = props.lat;
+//     const lon = props.lon;
+
+//     let [ mapSrc, setMapSrc] = useState();
+//     return ( <>
+//     <img src={mapSrc}></img>
+//     </> );
+// }
+
+// export default Map;
