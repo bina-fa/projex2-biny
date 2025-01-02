680ca97324a78591a1d643d8d9b89868c05c485d
Biny <rachelfa33@gmail.com>
2025-01-02 03:15:27 +0200

    Add Documentation

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,49 +8,53 @@ function Form() {
 
    let { register, reset, handleSubmit, formState: { errors, isValid }} = useForm();
    let [ data, setData] = useState([{}]);
-   let [ lat, setLat] = useState(31.80461649366907);
+   let [ lat, setLat] = useState(31.80461649366907);//איפוס הסטיייט בכתובת אחת
    let [ lon, setLon] = useState(34.66327724630701);
  
- function save(){
+ function save(){ // פונקציה שתשמור בעתיד את המידע מהטופס
     reset()
  }
 
 
-  const ChangeAdress = async(e) => {
+  const ChangeAdress = async(e) => { 
    const {value} = e.target;
    console.log( value );
 
-       const response = await fetch( `https://nominatim.openstreetmap.org/search?format=json&q=${value}&limit=5 `)
-       const res = await response.json();
+        //שולחת בקשה לשרת על 7 כתובות המתאימות ביותר למידע שהוקלד בינפוט
+       const response = await fetch( `https://nominatim.openstreetmap.org/search?format=json&q=${value}&limit=7 `)
+        const res = await response.json(); //json המרת התשובה שהגיעה ל 
        console.log("res: " , res);
-       setData(res);
+       setData(res); // תעדכן את הסטייט 
 
        await console.log("data : " , data);       
  }
 
 
     return ( 
-         <div id='father'>
+      //in div-father there is two items: form & Map(component) 
+         <div id='father'> 
     <form noValidate onSubmit={handleSubmit(save)}>
 
+        {/* name input: */}
        <input type="text"{...register ( "name", { required : 'שדה שם הוא שדה חובה'})} id="nameInput" placeholder="enter name"></input> 
           
           {errors.name && <div className="error">{errors.name.message} </div>}
+          
 
-
+        {/* adress input: */}
        <input
         type="text"
          {...register ( "adress", { required : 'שדה כתובת הוא שדה חובה'  })}
         
          id ="adressInput"
-         list="options"
+         list="options" //connect to datalist with id "options"
           placeholder="adress"
-          onChange={ChangeAdress}  
-          onSelect={(e) => {
+          onChange={ChangeAdress} //עם שינוי תגש לפונקציה הקוראת למידע מהשרת עם הערך המעודכן שלנו 
+          onSelect={(e) => { //בזמן שבחרו משהו שקשור אליך(כלומר: לחצו על אחד האפשרויות בדאטאליסט)
             const selectedValue = e.target.value;
-            const selectedItem = data.find(itemm => itemm.display_name === selectedValue);
+            const selectedItem = data.find(itemm => itemm.display_name === selectedValue);//חפש את הכתובת ע"י שם הכתובת שנכנסת אוטומטית לינפוט
             if(selectedItem){
-              setLat(selectedItem.lat);
+              setLat(selectedItem.lat);//lot וה ,lon -ותעדכן את הסטייט של ה 
               setLon(selectedItem.lon);
               console.log("lat: ", selectedItem.lat,", lon: ", selectedItem.lon);
               
@@ -63,18 +67,20 @@ function Form() {
 
        <datalist id = "options">
         {data.map(
+          // option תעבור על כל הכתובות שהגיעו מהשרת (ונמצאים בסטייט )ושים את שם המקום בתוך 
           (item, index) => ( 
             <option key={item.osm_id + index} 
-            value = {item.display_name}
-            >
+            value = {item.display_name}>
               {item.display_name}
               </option>
         ) )}
        </datalist>
 
+            {/* phone input: */}
        <input type="phone" {...register ( "phone", { required :'שדה טלפון הוא שדה חובה'  })} id ="phoneInput" placeholder="phone"></input> 
        {errors.phone && <div className="error">{errors.phone.message} </div>}
 
+              {/* email input: */}
        <input type="email"
          {...register("email", {
          required : 'שדה אימייל הוא שדה חובה',
@@ -86,24 +92,31 @@ function Form() {
              id="emailInput" placeholder="Email"></input> 
         {errors.email && <div className="error">{errors.email.message} </div>}
 
+    {/* האם נדרש חיבור לאינטרנט? */}
        <label htmlFor="internet" >{"Need an internet connection?"}</label>
        <input type="Checkbox" name="internet" id="internet"/>
 
-       <label htmlFor="kitchen">{"Need a kitchen?"}</label> 
+    {/* האם נדרש מטבח? */}
+<label htmlFor="kitchen">{"Need a kitchen?"}</label> 
            <input type="Checkbox" name="kitchen" id="kitchen"/>
+
+    {/* האם צריך מכונת קפה? */}
 
        <label htmlFor="coffeeMachine"> {"Need a coffe machine?"}</label>
               <input type="Checkbox"name="coffeeMachine" id="coffeeMachine"/>
-
-       <input type="number" id="numRooms"  defaultValue = {2} placeholder="num rooms"></input> 
+            {/* מספר חדרים*/}
+       <input type="number" id="numRooms"  defaultValue = {2} placeholder="num rooms"></input>
        
+
        <input type="number" id="distance"  placeholder="distance"></input> 
 
+               {/* סטטוס */}
        <input id = "status" defaultValue = {"מחפש"}></input>
 
        <input type="submit" id="submitButton" value={"save"}/>
 
     </form>
+    {/* קריאה לקומפוננטה MAP ושליחת האלמנטים המעודכנים */}
     <Map key = {`${lat}-${lon}`} lat={lat} lon={lon} ></Map>
 
     </div>
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,5 +1,5 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';//אחרי התקנת הספריה ייבוא האלמנטים השימושיים באפליקציה זו
+import 'leaflet/dist/leaflet.css' // css ייבוא דף  חיצוני
 
 import './css/map.css'
 
@@ -9,15 +9,23 @@ function Map(props) {
     const {lon} = props;
 
     return ( <>
+    {/* center- זה הנקודות המובחרות שמציגות נקודה על המפה 
+        -----
+    zoom -מרחק וגדל הזום על הכתובת במפה
+    ----
+    scrollWheelZoom -  האם ניתן להגדיל את המפה לאחר הבחירה של האזור -להגדיל זום
+    */}
 
       <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" //בקשה לתמונת מפה
         />
+        {/* Marker הוא הסמן */}
         <Marker position={[lat, lon]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            {/* כשלוחצים על הסמן מוקפץ המשפט הבא */}
+           a good place. <br /> Easily customizable.
           </Popup>
         </Marker>
       </MapContainer>
